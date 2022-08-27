a9b1ff1a29d9e2ea2988901180d7f400f8f0aac1
patifnati <rzezniczekpatrycja@gmail.com>
2022-08-27 22:16:00 +0200

    done

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
-    }
+        if (20>18) {
+            System.out.println("true");
+         }  else {
+            System.out.println("false");
+        }
+
+         }
 }
